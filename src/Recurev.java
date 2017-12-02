e51f593e0346ba5606cecefd9f4e6da90ef34947
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-01 22:32:29 -0500

    added constructor for Recurev

--- src/Recurev.java
+++ src/Recurev.java
@@ -1,5 +1,8 @@
+import java.sql.Time;
+
 public class Recurev extends Timev {
-
-
+    public Recurev(Time startTime, Time endTime, String title, int priorityLevel){
+        super(startTime, endTime, title, priorityLevel);
+    }
 
 }

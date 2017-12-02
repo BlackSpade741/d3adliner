3d5fb5d610e85a595108e5728bd2255aef1152b8
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-02 18:28:16 -0500

    modified NewSpareEvent

--- app/src/main/java/net/d3adliner/d3adliner/model/Controller.java
+++ app/src/main/java/net/d3adliner/d3adliner/model/Controller.java
@@ -1,6 +1,7 @@
 package net.d3adliner.d3adliner.model;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class Controller {
     private ArrayList<Reminder> reminders;
@@ -29,28 +30,28 @@ public class Controller {
     }
     public void NewSpareEvent(Event OSpare,Event event){
         Event Nspare=null;
-        int[] date = event.getNumDate();
-            if(event.getStartTime().getHours()==OSpare.getStartTime().getHours()) {
-                Time start = event.getEndTime();
-                Time end  = OSpare.getEndTime();
-                Nspare = new Event(start,end,date[0],date[1],date[2],"SPARE TIME",false,"SPARE TIME");
-            }
-            else if(event.getEndTime().getHours()==OSpare.getEndTime().getHours()){
-                Time start = OSpare.getStartTime();
-                Time end  = event.getStartTime();
-                Nspare = new Event(start,end,date[0],date[1],date[2],"SPARE TIME",false,"SPARE TIME");
-            }
-            else{
-                Time start = OSpare.getStartTime();
-                Time end  = event.getStartTime();
-                Nspare = new Event(start,end,date[0],date[1],date[2],"SPARE TIME",false,"SPARE TIME");
-                schedule.addEvent(Nspare);
-                start = event.getEndTime();
-                end  = OSpare.getEndTime();
-                Nspare = new Event(start,end,date[0],date[1],date[2],"SPARE TIME",false,"SPARE TIME");
-            }
-        schedule.addEvent(Nspare);
+        GregorianCalendar date = event.getEventDate();
+        if(event.getStartTime().getHours()==OSpare.getStartTime().getHours()) {
+            Time start = event.getEndTime();
+            Time end  = OSpare.getEndTime();
+            Nspare = new Event(start,end,date,"SPARE TIME",false,"SPARE TIME");
         }
+        else if(event.getEndTime().getHours()==OSpare.getEndTime().getHours()){
+            Time start = OSpare.getStartTime();
+            Time end  = event.getStartTime();
+            Nspare = new Event(start,end,date,"SPARE TIME",false,"SPARE TIME");
+        }
+        else{
+            Time start = OSpare.getStartTime();
+            Time end  = event.getStartTime();
+            Nspare = new Event(start,end,date,"SPARE TIME",false,"SPARE TIME");
+            schedule.addEvent(Nspare);
+            start = event.getEndTime();
+            end  = OSpare.getEndTime();
+            Nspare = new Event(start,end,date,"SPARE TIME",false,"SPARE TIME");
+        }
+        schedule.addEvent(Nspare);
+    }
     public ArrayList<AssignmentStages> createStages() {
         return null;
     }

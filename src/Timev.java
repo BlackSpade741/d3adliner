34a41c02899ea78b851ba712e0697afb5ddb06a9
Reece Martin <capt.rm.fae@gmail.com>
2017-12-01 21:51:08 -0500

    Added new classes.

--- src/Timev.java
+++ src/Timev.java
@@ -1,3 +1,12 @@
+import java.sql.Time;
+
 public class Timev{
+
+    private Time startTime = ;
+    private Time endTime = ;
+    private String title;
+    // priorityLevel runs from 0 - 9 : low - high
+    private  int priorityLevel;
+
 
 }

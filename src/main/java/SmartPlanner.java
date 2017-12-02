140fd3803522c4ee565d480c3124508ec5c83f6d
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-02 15:41:19 -0500

    added constructor

--- src/main/java/SmartPlanner.java
+++ src/main/java/SmartPlanner.java
@@ -1,4 +1,10 @@
 package main.java;
 
 public class SmartPlanner {
+    private Timeline schedule;
+    private  Controller controller;
+    public SmartPlanner(Controller controller, Timeline schedule) {
+        this.controller = controller;
+        this.schedule = schedule;
+    }
 }

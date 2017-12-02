362b588878a2d26e0344514742e6a961058d0aca
Ellen Chen <chenellen007@gmail.com>
2017-12-02 14:55:19 -0500

    Added class attributes to RecurringEvent

--- src/main/java/RecurringEvent.java
+++ src/main/java/RecurringEvent.java
@@ -2,12 +2,24 @@ package main.java;
 
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class RecurringEvent {
-    Time timeToRun;
+    private Time startTime;
+    private Time endTime;
+    private GregorianCalendar startDate;
+    private GregorianCalendar endDate;
+    private int repeatInterval;
+    private String repeatType;
+    private static final String[] REPEAT_TYPES = new String[]{"DAYS", "MONTHS", "WEEKS"};
 
-    public RecurringEvent(Time timeToRun) {
-        this.timeToRun = timeToRun;
+    private List<Timev> listOfEvents;
+
+    public RecurringEvent(){}
+
+    public RecurringEvent(Time startTime, Time endTime, GregorianCalendar startDate,
+                          GregorianCalendar endDate, int repeatInterval, String repeatType){
+        
     }
-
 }

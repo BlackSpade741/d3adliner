8f92611a2d65efd3bcef3ad90b9dcafd4ce30289
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-02 13:46:24 -0500

    deleted sql.Time and fixed its corresponding methods

--- src/main/java/Timeline.java
+++ src/main/java/Timeline.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 public class Timeline{
 
     private ArrayList<Timev> events;
+    private Timev curevent;
     public Timeline(){
         events = new ArrayList<Timev>();
     }
     public void addEvent(Timev event){
+        if(events.size()==0){
+            curevent=event;
+        }
         events.add(event);
     }
     public void removeEvent(Timev event){
@@ -15,6 +19,17 @@ public class Timeline{
     }
     public ArrayList<Timev> getEvents(){
         return new ArrayList<>(events);
+    }
+    public ArrayList<Timev> getEventInADay () {
+        Double hour = 0.0;
+        ArrayList<Timev> DailyEvent = new ArrayList<>();
+        for (Timev event : events) {
+            hour+=event.getDuration();
+            if(hour<=24.0){
+                DailyEvent.add(event);
+            }
+        }
+        return DailyEvent;
     }
 
 }
--- src/main/java/Timev.java
+++ src/main/java/Timev.java
@@ -1,21 +1,25 @@
 package main.java;
-import java.sql.Time;
 
 public class Timev{
+    private static String[] EventTypes={"LECTURE","SPARE TIME","FOOD"};
     private Time startTime;
     private Time endTime;
     private String title;
+    private Time duration;
     // priorityLevel runs from 0 - 9 : low - high
     private  int priorityLevel;
+    private String type;
     public Timev(){}
 
-    public Timev(Time startTime, Time endTime,String title, int priorityLevel){
+    public Timev(Time startTime, Time endTime,String title, int priorityLevel, String type){
         this.startTime = startTime;
         this.endTime = endTime;
         this.title = title;
         this.priorityLevel = priorityLevel;
+        this.type = type;
+        this.duration = new Time(Double.toString(Double.parseDouble(endTime.toString())-Double.parseDouble(startTime.toString())));
     }
-    public Double getDuration(){
-        return Double.parseDouble(endTime.toString())-Double.parseDouble(startTime.toString());
+    public Time getDuration(){
+        return duration;
     }
 }

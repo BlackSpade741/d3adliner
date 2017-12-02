e3c7af0c903fcc63c5fccaae0aa619d6a304e3b7
Reece Martin <capt.rm.fae@gmail.com>
2017-12-02 13:59:48 -0500

    Added date to time event.

--- src/main/java/Timev.java
+++ src/main/java/Timev.java
@@ -1,9 +1,12 @@
 package main.java;
 
+import java.util.Date;
+
 public class Timev {
     private static String[] EventTypes = {"LECTURE", "SPARE TIME", "FOOD"};
     private Time startTime;
     private Time endTime;
+    private Date eventDate;
     private String title;
     private Time duration;
     // priorityLevel runs from 0 - 9 : low - high
@@ -13,13 +16,14 @@ public class Timev {
     public Timev() {
     }
 
-    public Timev(Time startTime, Time endTime, String title, int priorityLevel, String type) {
+    public Timev(Time startTime, Time endTime, int year, int month, int day, String title, int priorityLevel, String type) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.title = title;
         this.priorityLevel = priorityLevel;
         this.type = type;
         this.duration = new Time(Double.toString(Double.parseDouble(endTime.toString()) - Double.parseDouble(startTime.toString())));
+        this.eventDate = new Date(year, month, day);
     }
 
     public Time getDuration() {

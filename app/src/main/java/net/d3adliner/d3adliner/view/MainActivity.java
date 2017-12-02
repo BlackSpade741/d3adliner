685f42d371b9faa55d428dd15c6420a17c2a4c8c
Ellen Chen <chenellen007@gmail.com>
2017-12-02 16:17:20 -0500

    Moved MainActivity.java to package view

--- app/src/main/java/net/d3adliner/d3adliner/view/MainActivity.java
+++ app/src/main/java/net/d3adliner/d3adliner/view/MainActivity.java
@@ -1,7 +1,9 @@
-package net.d3adliner.deadliner;
+package net.d3adliner.d3adliner.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.d3adliner.deadliner.R;
 
 public class MainActivity extends AppCompatActivity {
 

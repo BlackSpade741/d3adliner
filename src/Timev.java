c19c714b549652d3c12b3c43868e69d7b4c2ebfe
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-01 22:26:22 -0500

    added constructor for Timev

--- src/Timev.java
+++ src/Timev.java
@@ -1,12 +1,17 @@
 import java.sql.Time;
 
 public class Timev{
-
-    private Time startTime = ;
-    private Time endTime = ;
+    private Time startTime;
+    private Time endTime;
     private String title;
     // priorityLevel runs from 0 - 9 : low - high
     private  int priorityLevel;
+    public Timev(Time startTime, Time endTime,String title, int priorityLevel){
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.title = title;
+        this.priorityLevel = priorityLevel;
+    }
 
 
 }

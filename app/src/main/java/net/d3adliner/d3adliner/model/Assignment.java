2ac5fd67e7573a4ceda8aabb4dbe43b76f7c7aaa
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-03 00:32:20 -0500

    added a GregorianCalender and method getReminder

--- app/src/main/java/net/d3adliner/d3adliner/model/Assignment.java
+++ app/src/main/java/net/d3adliner/d3adliner/model/Assignment.java
@@ -1,21 +1,32 @@
 package net.d3adliner.d3adliner.model;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Timer;
 
 public class Assignment {
     private ArrayList<AssignmentStages> Stages;
     private AssignmentStages curStage;
-    private Time dueDate;
+    private Time dueTime;
     private String name;
     private boolean Complete = false;
+    private GregorianCalendar dueDate;
 
-    public Assignment(ArrayList<AssignmentStages> Stages, Time dueDate, String name) {
+    public Assignment(ArrayList<AssignmentStages> Stages, GregorianCalendar dueDate, Time dueTime, String name) {
         this.Stages = Stages;
         this.curStage = this.Stages.get(0);
         this.dueDate = dueDate;
         this.name = name;
+        this.dueTime=dueTime;
     }
-
+    public void getReminder(){
+        long min = dueDate.getTimeInMillis()+((long)dueTime.getHours()*60*1000*60)+
+                ((long)dueTime.getMinutes()*60*1000)-minutes;
+        Date date = new Date(min);
+        Timer t = new Timer();
+        t.schedule(new Reminder(),date);
+    }
     public void CheckStage() {
         if (curStage.CheckComplete()) {
             if (!(Stages.indexOf(curStage) == Stages.size() - 1)) {

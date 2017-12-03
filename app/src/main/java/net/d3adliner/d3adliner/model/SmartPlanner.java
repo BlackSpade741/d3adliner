0767dad8e0a773b96dc010fadd0612df7a2afaeb
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-02 19:12:41 -0500

    redefined constructor and added AddInitialEvents

--- app/src/main/java/net/d3adliner/d3adliner/model/SmartPlanner.java
+++ app/src/main/java/net/d3adliner/d3adliner/model/SmartPlanner.java
@@ -1,11 +1,21 @@
 package net.d3adliner.d3adliner.model;
 
+import java.util.ArrayList;
+
 public class SmartPlanner {
     private Timeline schedule;
     private  Controller controller;
-    public SmartPlanner(Controller controller, Timeline schedule) {
+    private ArrayList<Event> Initials;
+    public SmartPlanner(Controller controller, Timeline schedule, ArrayList<Event> Initials) {
         this.controller = controller;
         this.schedule = schedule;
+        this.Initials = Initials;
+        AddInitialEvents();
+    }
+    public void AddInitialEvents(){
+        for(Event ev:Initials){
+            controller.addEvent(ev);
+        }
     }
     public void Plan(){}
 }

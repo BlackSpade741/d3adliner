7147a54acf805c5316144853b0f70c403569a0ae
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-02 20:57:28 -0500

    extended TimerTask and defined overried method run()

--- app/src/main/java/net/d3adliner/d3adliner/model/Reminder.java
+++ app/src/main/java/net/d3adliner/d3adliner/model/Reminder.java
@@ -1,7 +1,13 @@
 package net.d3adliner.d3adliner.model;
 
-public class Reminder {
+import java.util.TimerTask;
+
+public class Reminder extends TimerTask{
     public Reminder() {
     }
 
+    @Override
+    public void run() {
+
+    }
 }

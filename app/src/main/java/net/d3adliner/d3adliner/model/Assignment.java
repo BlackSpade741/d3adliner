7f355ea324f3a542857465276560b2bf05684910
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-03 01:29:47 -0500

    added method dueMsg

--- app/src/main/java/net/d3adliner/d3adliner/model/Assignment.java
+++ app/src/main/java/net/d3adliner/d3adliner/model/Assignment.java
@@ -6,19 +6,21 @@ import java.util.GregorianCalendar;
 import java.util.Timer;
 
 public class Assignment {
-    private ArrayList<AssignmentStages> Stages;
+    private ArrayList<AssignmentStages> Stages = new ArrayList<>();
     private AssignmentStages curStage;
     private Time dueTime;
     private String name;
     private boolean Complete = false;
     private GregorianCalendar dueDate;
 
-    public Assignment(ArrayList<AssignmentStages> Stages, GregorianCalendar dueDate, Time dueTime, String name) {
-        this.Stages = Stages;
+    public Assignment(GregorianCalendar dueDate, Time dueTime, String name) {
         this.curStage = this.Stages.get(0);
         this.dueDate = dueDate;
         this.name = name;
         this.dueTime=dueTime;
+    }
+    public void addStage(AssignmentStages stage){
+        Stages.add(stage);
     }
     public void getReminder(int minutes){
         long min = dueDate.getTimeInMillis()+((long)dueTime.getHours()*60*1000*60)+
@@ -27,6 +29,14 @@ public class Assignment {
         Timer t = new Timer();
         t.schedule(new Reminder(),date);
     }
+    public void dueMsg(){
+        long min = dueDate.getTimeInMillis()+((long)dueTime.getHours()*60*1000*60)+
+                ((long)dueTime.getMinutes()*60*1000);
+        Date date = new Date(min);
+        Timer t = new Timer();
+        t.schedule(new Reminder(),date);
+    }
+
     public void CheckStage() {
         if (curStage.CheckComplete()) {
             if (!(Stages.indexOf(curStage) == Stages.size() - 1)) {

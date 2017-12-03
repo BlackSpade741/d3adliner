37eae11b758230e25f13ecdf1e325b8fe35ad421
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2017-12-03 01:31:21 -0500

    added method addAssignnment

--- app/src/main/java/net/d3adliner/d3adliner/model/SmartPlanner.java
+++ app/src/main/java/net/d3adliner/d3adliner/model/SmartPlanner.java
@@ -3,6 +3,7 @@ package net.d3adliner.d3adliner.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.GregorianCalendar;
 
 public class SmartPlanner {
     private Timeline schedule;
@@ -22,6 +23,14 @@ public class SmartPlanner {
     }
     public void Plan(){
         ArrayList events = schedule.getEvents();
+
         // events.sort((Comparator<? super T>) new CustomComparator());
+    }
+    public void addAssignment(GregorianCalendar dueDate,Time dueTime,String name){
+        Assignment a = new Assignment(dueDate,dueTime,name);
+        String t = Double.toString(dueTime.getHours()+1)+Double.toString(dueTime.getMinutes());
+        int day=(int)(dueDate.getTimeInMillis()/(1000*60*60*24)-6);
+        Event e1 = new Event(dueTime,new Time(t),new GregorianCalendar(1970,0,day),name, true,"ASSIGNMENT");
+        controller.addEvent(e1);
     }
 }
